ed0bfd5ec7c7f9efb0380c81916707b539178585
Patrik Bikes <patrik.bikes@yahho.com>
2017-12-05 16:36:18 +0100

    week 3 day 2 created understandable variables in lottery task

--- week-03/day-02/11_lottery.py
+++ week-03/day-02/11_lottery.py
@@ -1,16 +1,16 @@
 # Create a method that find the 5 most common lottery numbers otos.csv
 
 def five_most_frequent(file_name):
-    dict = {}
-    with open(file_name) as fo:
-        for line in fo.readlines():
+    lottery_dictionary = {}
+    with open(file_name) as open_file:
+        for line in open_file.readlines():
             for i in range(5):
-                if not line.rstrip("\n").split(";")[11+i] in dict:
-                    dict[line.rstrip("\n").split(";")[11+i]] = 1
+                if not line.rstrip("\n").split(";")[11+i] in lottery_dictionary:
+                    lottery_dictionary[line.rstrip("\n").split(";")[11+i]] = 1
                 else:
-                    dict[line.rstrip("\n").split(";")[11+i]] +=1
-        s_dict = [(k, dict[k]) for k in sorted(dict, key=dict.get, reverse=True)]
-        return s_dict[:5]
+                    lottery_dictionary[line.rstrip("\n").split(";")[11+i]] +=1
+        sorted_dictionary = [(k, lottery_dictionary[k]) for k in sorted(lottery_dictionary, key=lottery_dictionary.get, reverse=True)]
+        return sorted_dictionary[:5]
 
 file_name = "otos.csv"
 print(five_most_frequent(file_name))
