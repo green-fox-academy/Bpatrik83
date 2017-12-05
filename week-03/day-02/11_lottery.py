462032bf97a1d31c181aaa9067ab41bf8f9d634a
Patrik Bikes <patrik.bikes@yahho.com>
2017-12-05 15:55:39 +0100

    week 3 day 2 corrected lottery

--- week-03/day-02/11_lottery.py
+++ week-03/day-02/11_lottery.py
@@ -5,10 +5,10 @@ def five_most_frequent(file_name):
     with open(file_name) as fo:
         for line in fo.readlines():
             for i in range(5):
-                if not line.split(";")[11+i] in dict:
-                    dict[line.split(";")[11+i]] = 0
+                if not line.rstrip("\n").split(";")[11+i] in dict:
+                    dict[line.rstrip("\n").split(";")[11+i]] = 1
                 else:
-                    dict[line.split(";")[11+i]] += 1
+                    dict[line.rstrip("\n").split(";")[11+i]] +=1
         s_dict = [(k, dict[k]) for k in sorted(dict, key=dict.get, reverse=True)]
         return s_dict[:5]
 
