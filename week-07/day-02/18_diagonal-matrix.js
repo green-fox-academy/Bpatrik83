fc1c96ec001c734552355a9ff56e6ac87a1eec23
Patrik Bikes <patrik.bikes@yahho.com>
2018-01-09 17:33:00 +0100

    refactor diagonal-matrix

--- week-07/day-02/18_diagonal-matrix.js
+++ week-07/day-02/18_diagonal-matrix.js
@@ -16,14 +16,11 @@ let arr = [[]];
 let size = 4
 
 for(let i = 0; i < size; i++){
+    let temp = '';
     arr[i] = [0];
     for(let j = 0; j < size; j++){
-        if (i + j === size - 1) {
-            arr[i][j] = [1];
-        } else {
-            arr[i][j] = [0];
-        }
+        temp += i + j === size - 1 ? arr[i][j] = [1] + ' ' : arr[i][j] = [0] + ' ';
     }
+    console.log(temp);
 }
 
-console.log(arr);
