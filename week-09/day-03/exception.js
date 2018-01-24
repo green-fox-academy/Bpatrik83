ada1ce46d91462d8be73b59df1531fd17840c0b3
Bpatrik83 <patrik.bikes@yahoo.com>
2018-01-24 11:04:01 +0100

    exception task done

---
+++ week-09/day-03/exception.js
@@ -0,0 +1,26 @@
+'use strict';
+
+// Handle the exceptions in the addString() function. It should check the type of the
+// arguments, throw the right error and write it to the console.
+// It should add the strings too if the arguments are appropriate.
+
+let  addString = function(str1, str2, printStr){
+  if (typeof a !== 'String'){
+    throw new Error('"str1" is not a String');
+  } else if (typeof b !== 'String'){
+    throw new Error('"str2" is not a String');
+  }
+  let newStr = str1 + str2;
+  printStr(newStr);
+}
+
+let printStr = function(str) {
+  console.log(str);
+}
+
+try {
+  addString(1234, 56789, 'printStr');
+} catch (err) {
+  console.log('catching error:');
+  console.log(err.message);
+}
