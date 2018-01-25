2bc62d4b649a38c9323ea68f6523a3919af6d9ce
Bpatrik83 <patrik.bikes@yahoo.com>
2018-01-25 12:35:00 +0100

    doubling task done

--- week-09/day-04/index.js
+++ week-09/day-04/index.js
@@ -14,16 +14,21 @@ app.get('/', function (req, res) {
 });
 
 app.get('/doubling', function (req, res) {
-  if (req.query.input !== 'undefined') {
+  console.log(req.query.input);
+  if (typeof req.query.input !== 'undefined') {
     res.json({
-      "received": req.query.input,
-      "result": req.query.input * 2
+      "received": Number(req.query.input),
+      "result": Number(req.query.input) * 2
     });
   } else {
     res.json({
       "error": "Please provide an input!"
     });
   }
+});
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
 });
 
 app.listen(8080, function () {
